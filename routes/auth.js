341628df9e93ab47cbceac0cedaf233371d3ac10
akinniyi1 <64546693+akinniyi1@users.noreply.github.com>
2025-06-22 10:06:14 +0100

    Delete routes directory

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerSeller } = require("../controllers/authController");
-
-router.post("/register", registerSeller);
-
-module.exports = router;
