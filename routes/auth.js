d8031d621288d8494da4a6bfef894b2093113060
akinniyi1 <64546693+akinniyi1@users.noreply.github.com>
2025-06-22 10:05:27 +0100

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { registerSeller } = require("../controllers/authController");
+
+router.post("/register", registerSeller);
+
+module.exports = router;
