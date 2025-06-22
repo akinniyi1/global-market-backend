a225256815c8c00825cef22c6e4f11e0758df833
akinniyi1 <64546693+akinniyi1@users.noreply.github.com>
2025-06-22 11:09:27 +0100

    Create routes-products.js

---
+++ routes-products.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const multer = require("multer");
+const router = express.Router();
+const { postProduct, getAllProducts } = require("./controllers-productController");
+
+// Use multer to handle file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post("/create", upload.single("media"), postProduct);
+router.get("/all", getAllProducts);
+
+module.exports = router;
